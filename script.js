6da7a8de4732450e2db22fd723073971015db6eb
Ze Loong <teohzeloong12@gmail.com>
2023-07-08 23:47:35 +0800

    added sketch and clear

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function makeRows(x){
             const size = 360/x
             column.style.width = `${size}px`;
             column.style.height = `${size}px`;
-            console.log(size)
+            column.classList.add("block");
             row.appendChild(column)
         }
         container.appendChild(row);
@@ -22,3 +22,29 @@ function makeRows(x){
 }
 
 makeRows(8);
+
+const blocks = document.querySelectorAll('.block');
+const getClear = document.querySelector('.clear');
+
+function sketch(){
+    blocks.forEach((block) => {
+        block.addEventListener("mouseover", () => {
+            block.style.backgroundColor = "black";
+        })
+    })
+}
+
+function clear(){
+    blocks.forEach((block) => {
+        getClear.addEventListener("click", () => {
+            block.style.backgroundColor = "white";
+        })
+    })
+}
+
+sketch()
+clear()
+
+function blockEdit(block, colour){
+    block.style.backgroundColor = colour
+}
